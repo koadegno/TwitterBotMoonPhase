348c6c7987741aed59a14444e9eb42ac45c42cf3
koadegno <vinove9@hotmail.fr>
2022-02-15 12:48:55 +0100

    comment

--- MoonPhase.py
+++ MoonPhase.py
@@ -26,7 +26,6 @@ import datetime
 MONTHS = ["janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre"]
 PHASE_ID = ["New Moon","First Quarter","Full Moon","Last Quarter"]
 NUMBER_OF_PHASE = 4
-MAX_DAYS_TO_TAKE = 3
 
 class MoonPhase():
 	"""
@@ -90,25 +89,42 @@ class MoonPhase():
 			self.moon_phase_dico[lunation_number] = items
 		
 	def _build_day_moon_phase(self,lunation_nb,days_left,date,phase_id):
+		"""
+		lunation_nb : int 
+		days_left	: int
+		date 		: datetime type
+		phase_id	: int 
+		"""
 		return (lunation_nb,days_left,date,PHASE_ID[phase_id])
 
-	def moon_phase_of_the_day():
-		pass
+	def moon_phase_of_the_day(self):
+		"""
+		return information of today using day_moon_phase function
+		"""
+		return self.day_moon_phase(datetime.date.today(),1)
 
-
-	def day_moon_phase(self,date_time):
-		date_time = datetime.date.today()+datetime.timedelta(days=1)
+	def day_moon_phase(self,date_time,max_days_to_take):
+		"""
+		date_time 		 : datetime type the date information is necessary to know
+		max_days_to_take : int type the number of phase to take
+		"""
 		next_phase_date = -1
 		days_take = 0
+		res = []
+
 		for lun_number, date_moon_phase in self.moon_phase_dico.items():
 			for phase_id in range(NUMBER_OF_PHASE):
 				phase_day = date_moon_phase[phase_id]
-				days_left = phase_day.date() - date_time
+				days_left = phase_day.date() - date_time # calculate the nb of days left
 
-				if days_left.days > next_phase_date and days_take < MAX_DAYS_TO_TAKE:
-					print(self._build_day_moon_phase(lun_number,days_left.days,phase_day,phase_id))
+				# if days left > next_phase ( init = -1) ==> take it
+				if days_left.days > next_phase_date:
+					res.append(self._build_day_moon_phase(lun_number,days_left.days,phase_day,phase_id))
 					next_phase_date = days_left.days
 					days_take += 1
+				
+				if days_take == max_days_to_take:
+					return res
 
 
 
@@ -117,4 +133,4 @@ if __name__ == "__main__":
 	year = datetime.datetime.today().year
 	print(year)
 	moon_phase = MoonPhase(year)
-	moon_phase.day_moon_phase("ca")
+	print(moon_phase.moon_phase_of_the_day())
